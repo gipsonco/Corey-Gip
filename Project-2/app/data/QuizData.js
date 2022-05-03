08126bd0526b3db3d018fe7927d4a945868aa8de
Corey <gipsonc@gmatc.matc.edu>
2022-05-02 21:00:04 -0500

    Finished Project-2

---
+++ Project-2/app/data/QuizData.js
@@ -0,0 +1,52 @@
+export default data = [
+    {
+        question: "How many All Star Appearances does Kobe Bryant have?",
+        options: ["16","17","18","19"],
+        correct_option: "18"
+    },
+    {
+        question: "What city won the first NBA Championship?",
+        options: ["Chicago","Los Angeles","Baltimore", "Philadelphia"],
+        correct_option: "Philadelphia"
+    },
+    {
+        question: "Who was the hardest player to guard for Kobe Bryant?",
+        options: ["Tracy McGrady","Kevin Durant","Allen Iverson","Michael Jordan"],
+        correct_option: "Kevin Durant"
+    },
+    {
+        question: "Who was Michael Jordan toughest defender?",
+        options: ["Gary Payton","Joe Dumars","Dennis Rodman","Isaiah Thomas"],
+        correct_option: "Joe Dumars"
+    },
+    {
+        question: "What year was the Milwaukee Bucks 1st Championship?",
+        options: ["1970","1971","1972","1974"],
+        correct_option: "1972"
+    },
+    {
+        question: "Who scored the most career points in NBA History?",
+        options: ["Lebron James","Kobe Bryant","Michael Jordan","Kareem Abdul-Jabbar"],
+        correct_option: "Kareem Abdul-Jabbar"
+    },
+    {
+        question: "What year did Giannis win his 1st MVP?",
+        options: ["2019","2020","2018","2021"],
+        correct_option: "2019"
+    },
+    {
+        question: "What was Kobe Bryant middle name?",
+        options: ["Bean","Michael","Robert","Nathaniel"],
+        correct_option: "Bean"
+    },
+    {
+        question: "What is the most points Shaquille ONeal scored in a game?",
+        options: ["52","61","55","63"],
+        correct_option: "61"
+    },
+    {
+        question: "Who was the number 1 pick in 1984?",
+        options: ["Hakeem Olajuwon","Michael Jordan","John Stockton","Charles Barkley"],
+        correct_option: "Hakeem Olajuwon"
+    },
+]
